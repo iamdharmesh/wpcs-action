610b17a92ffa2b721ddf42372c6fd3ba59482d55
Konstantinos Galanakis <kmgalanakis@gmail.com>
2023-08-14 18:55:10 +0300

    fasfa sdfasdfasdfa

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,8 +28,7 @@ else
 fi
 
 git clone https://github.com/olivertappin/phpcs-diff.git ~/phpcs-diff
-cd ~/phpcs-diff
-composer install;
+~/phpcs-diff/comopser install
 ln -s ~/phpcs-diff/bin/phpcs-diff /usr/bin/phpcs-diff
 
 if [ -z "${INPUT_EXCLUDES}" ]; then
