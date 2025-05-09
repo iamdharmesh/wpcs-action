3d72c099fc0a5bee28c7c3b86765cbf89ffa76e2
Dharmesh Patel <dspatel44@gmail.com>
2025-05-09 17:40:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ if ! [ -x "$(command -v composer)" ]; then
 fi
 
 composer global config allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
-# composer global require --dev phpcsstandards/phpcsextra:"^1.2.0"
 composer global require --dev wp-coding-standards/wpcs:"^3.1.0" --update-with-all-dependencies
 
 git config --global --add safe.directory $(pwd)
@@ -108,31 +107,29 @@ if [ "${INPUT_ONLY_CHANGED_FILES}" = "true" ]; then
 else
     echo "Will check all files"
 fi
+COMPOSER_HOME="$(composer config home)/vendor"
 WPCS_PATH="$(composer config home)/vendor/wp-coding-standards/wpcs,$(composer config home)/vendor/phpcsstandards/phpcsutils,$(composer config home)/vendor/phpcsstandards/phpcsextra"
 if [ "${INPUT_STANDARD}" = "WordPress-VIP-Go" ] || [ "${INPUT_STANDARD}" = "WordPressVIPMinimum" ]; then
     echo "Setting up VIPCS"
-    git clone --depth 1 -b 2.3.3 https://github.com/Automattic/VIP-Coding-Standards.git ${HOME}/vipcs
-    git clone https://github.com/sirbrillig/phpcs-variable-analysis ${HOME}/variable-analysis
+    composer require --dev automattic/vipwpcs -W
+    composer require --dev sirbrillig/phpcs-variable-analysis -W
 
-    decide_all_files_or_changed "${WPCS_PATH},${HOME}/vipcs,${HOME}/variable-analysis"
+    decide_all_files_or_changed "${WPCS_PATH},${COMPOSER_HOME}automattic/vipwpcs,${COMPOSER_HOME}sirbrillig/phpcs-variable-analysis"
 elif [ "${INPUT_STANDARD}" = "10up-Default" ]; then
     echo "Setting up 10up-Default"
-    git clone https://github.com/10up/phpcs-composer ${HOME}/10up
-    git clone https://github.com/PHPCompatibility/PHPCompatibilityWP ${HOME}/phpcompatwp
-    git clone https://github.com/PHPCompatibility/PHPCompatibility ${HOME}/phpcompat
-    git clone https://github.com/PHPCompatibility/PHPCompatibilityParagonie ${HOME}/phpcompat-paragonie
-    git clone --depth 1 --branch 1.0.11 https://github.com/PHPCSStandards/PHPCSUtils ${HOME}/phpcsutils
-    git clone https://github.com/Automattic/VIP-Coding-Standards ${HOME}/vipcs
-    git clone https://github.com/sirbrillig/phpcs-variable-analysis ${HOME}/variable-analysis
+    composer require --dev 10up/phpcs-composer:"^3.0" -W
+    composer require --dev phpcompatibility/phpcompatibility-wp:"*" -W
+    composer require --dev phpcompatibility/phpcompatibility-paragonie:"*" -W
+    composer require --dev automattic/vipwpcs -W
+    composer require --dev sirbrillig/phpcs-variable-analysis -W
 
-    decide_all_files_or_changed "${WPCS_PATH},${HOME}/10up/10up-Default,${HOME}/phpcompatwp/PHPCompatibilityWP,${HOME}/phpcompat/PHPCompatibility,${HOME}/phpcompat-paragonie/PHPCompatibilityParagonieSodiumCompat,${HOME}/phpcompat-paragonie/PHPCompatibilityParagonieRandomCompat,${HOME}/vipcs,${HOME}/variable-analysis"
+    decide_all_files_or_changed "${WPCS_PATH},${COMPOSER_HOME}/10up/phpcs-composer/10up-Default,${COMPOSER_HOME}/phpcompatibility/phpcompatibility-wp/PHPCompatibilityWP,${COMPOSER_HOME}/phpcompatibility/phpcompatibility/PHPCompatibility,${COMPOSER_HOME}phpcompatibility/phpcompatibility-paragonie/PHPCompatibilityParagonieSodiumCompat,${COMPOSER_HOME}phpcompatibility/phpcompatibility-paragonie/PHPCompatibilityParagonieRandomCompat,${COMPOSER_HOME}automattic/vipwpcs,${COMPOSER_HOME}sirbrillig/phpcs-variable-analysis"
 
     # Add the phpcs -i command to list installed standards
     echo "Installed coding standards:"
     phpcs -i
 elif [ -z "${INPUT_STANDARD_REPO}" ] || [ "${INPUT_STANDARD_REPO}" = "false" ]; then
   echo "Setting up default WPCS"
-  git clone --depth 1 --branch 1.0.11 https://github.com/PHPCSStandards/PHPCSUtils ${HOME}/phpcsutils
   decide_all_files_or_changed "${WPCS_PATH}"
 else
   echo "Standard repository: ${INPUT_STANDARD_REPO}"
